0a4bc1d3a7e03bdfdde83716124622dff1503e30
Jai <apple@Apples-MacBook-Air-3.local>
2022-03-04 19:56:36 +0530

    completed with navbar

---
+++ components/footer/index.tsx
@@ -0,0 +1,12 @@
+import { IoIosMenu, IoLogoApple, IoMdBriefcase } from 'react-icons/io'
+
+export default function Index() {
+  const handleClick = () => {
+    alert('clicked me')
+  }
+  return (
+    <>
+      <h1>I am footer</h1>
+    </>
+  )
+}
---
+++ components/header/index.tsx
@@ -0,0 +1,35 @@
+import { IoIosMenu, IoLogoApple, IoMdBriefcase } from 'react-icons/io'
+
+export default function Index() {
+  const handleClick = () => {
+    alert('clicked me')
+  }
+  return (
+    <>
+      <div className="Owncss flex justify-around py-2 lg:hidden">
+        <IoIosMenu size="2rem" color="#fff" onClick={handleClick} />
+        <IoLogoApple size="2rem" color="#fff" />
+        <IoMdBriefcase size="2rem" color="#fff" />
+      </div>
+      <div className="Owncss hidden justify-evenly py-2 lg:flex">
+        <IoLogoApple size="2rem" color="#fff" />
+        <p className="textfont mt-1">store</p>
+        <p className="textfont mt-1 ">Mac</p>
+        <p className="textfont mt-1 ">iPad</p>
+        <p className="textfont mt-1 ">store</p>
+        <p className="textfont mt-1">Mac</p>
+        <p className="textfont mt-1 ">iPad</p>
+      </div>
+      <style jsx>{`
+        .Owncss {
+          background: rgba(0, 0, 0, 0.8);
+        }
+        .textfont {
+          font-family: 'SF Pro Text', 'Myriad Set Pro', 'SF Pro Icons',
+            'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+          color: #f5f5f7;
+        }
+      `}</style>
+    </>
+  )
+}
